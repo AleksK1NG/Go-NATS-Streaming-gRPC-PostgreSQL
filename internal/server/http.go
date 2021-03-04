fdb78f4426e91e3ea15b6951f59b77739b9fcea9
Alexander Bryksin <alexander.bryksin@yandex.ru>
2021-03-04 12:41:26 +0300

    feat: add https echo server

---
+++ internal/server/http.go
@@ -0,0 +1,59 @@
+package server
+
+import (
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+func (s *server) runHttpServer() {
+	s.echo.GET("/health", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Ok")
+	})
+	s.echo.GET("/metrics", echo.WrapHandler(promhttp.Handler()))
+	s.mapRoutes()
+
+	s.log.Infof("CFG: +v", s.cfg)
+
+	go func() {
+		s.echo.Server.ReadTimeout = time.Second * s.cfg.HTTP.ReadTimeout
+		s.echo.Server.WriteTimeout = time.Second * s.cfg.HTTP.WriteTimeout
+		s.echo.Server.MaxHeaderBytes = maxHeaderBytes
+		if err := s.echo.StartTLS(s.cfg.HTTP.Port, certFile, keyFile); err != nil {
+			s.log.Fatalf("Error starting TLS Server: ", err)
+		}
+	}()
+}
+
+func (s *server) mapRoutes() {
+	// docs.SwaggerInfo.Version = "1.0"
+	// docs.SwaggerInfo.Title = "Products microservice"
+	// docs.SwaggerInfo.Description = "Products REST API microservice."
+	// docs.SwaggerInfo.Version = "1.0"
+	// docs.SwaggerInfo.BasePath = "/api/v1"
+	// s.echo.GET("/swagger/*", echoSwagger.WrapHandler)
+	s.echo.Use(middleware.Logger())
+	s.echo.Pre(middleware.HTTPSRedirect())
+	s.echo.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderXRequestID, csrfTokenHeader},
+	}))
+	s.echo.Use(middleware.RecoverWithConfig(middleware.RecoverConfig{
+		StackSize:         stackSize,
+		DisablePrintStack: true,
+		DisableStackAll:   true,
+	}))
+	s.echo.Use(middleware.RequestID())
+	s.echo.Use(middleware.GzipWithConfig(middleware.GzipConfig{
+		Level: gzipLevel,
+		Skipper: func(c echo.Context) bool {
+			return strings.Contains(c.Request().URL.Path, "swagger")
+		},
+	}))
+	s.echo.Use(middleware.Secure())
+	s.echo.Use(middleware.BodyLimit(bodyLimit))
+}
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,11 +1,88 @@
 package server
 
 import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+
 	"github.com/AleksK1NG/nats-streaming/config"
 	"github.com/AleksK1NG/nats-streaming/pkg/logger"
+	"github.com/go-redis/redis/v8"
+	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/nats-io/stan.go"
+	"github.com/opentracing/opentracing-go"
+	"github.com/pkg/errors"
+
+	"github.com/labstack/echo/v4"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+const (
+	certFile        = "ssl/server.crt"
+	keyFile         = "ssl/server.pem"
+	maxHeaderBytes  = 1 << 20
+	gzipLevel       = 5
+	stackSize       = 1 << 10 // 1 KB
+	csrfTokenHeader = "X-CSRF-Token"
+	bodyLimit       = "2M"
+	kafkaGroupID    = "products_group"
 )
 
 type server struct {
-	log logger.Logger
-	cfg *config.Config
+	log      logger.Logger
+	cfg      *config.Config
+	natsConn stan.Conn
+	pgxPool  *pgxpool.Pool
+	tracer   opentracing.Tracer
+	echo     *echo.Echo
+	redis    *redis.Client
+}
+
+// NewServer constructor
+func NewServer(log logger.Logger, cfg *config.Config, natsConn stan.Conn, pgxPool *pgxpool.Pool, tracer opentracing.Tracer, redis *redis.Client) *server {
+	return &server{log: log, cfg: cfg, natsConn: natsConn, pgxPool: pgxPool, tracer: tracer, redis: redis, echo: echo.New()}
+}
+
+// Run start application
+func (s *server) Run() error {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// validate := validator.New()
+
+	go func() {
+		s.log.Infof("Server is listening on PORT: %s", s.cfg.HTTP.Port)
+		s.runHttpServer()
+	}()
+
+	metricsServer := echo.New()
+	go func() {
+		metricsServer.GET("/metrics", echo.WrapHandler(promhttp.Handler()))
+		s.log.Infof("Metrics server is running on port: %s", s.cfg.Metrics.Port)
+		if err := metricsServer.Start(s.cfg.Metrics.Port); err != nil {
+			s.log.Error(err)
+			cancel()
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+
+	select {
+	case v := <-quit:
+		s.log.Errorf("signal.Notify: %v", v)
+	case done := <-ctx.Done():
+		s.log.Errorf("ctx.Done: %v", done)
+	}
+
+	if err := s.echo.Server.Shutdown(ctx); err != nil {
+		return errors.Wrap(err, "echo.Server.Shutdown")
+	}
+
+	if err := metricsServer.Shutdown(ctx); err != nil {
+		s.log.Errorf("metricsServer.Shutdown: %v", err)
+	}
+
+	return nil
 }
