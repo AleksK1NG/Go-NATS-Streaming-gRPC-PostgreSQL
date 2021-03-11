81d1ae92e3291efc6c2cb189fee936bc4d431d45
Alexander Bryksin <alexander.bryksin@yandex.ru>
2021-03-11 17:59:49 +0300

    feat: add migrations update

--- migrations/01_create_initial_tables.up.sql
+++ migrations/01_create_initial_tables.up.sql
@@ -3,40 +3,34 @@ CREATE EXTENSION IF NOT EXISTS pg_trgm;
 CREATE EXTENSION IF NOT EXISTS btree_gist;
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 DROP TABLE IF EXISTS emails CASCADE;
-DROP TABLE IF EXISTS addresses CASCADE;
-
 
 CREATE TABLE emails
 (
-    email_id    UUID PRIMARY KEY         DEFAULT uuid_generate_v4(),
-    addressFrom VARCHAR(250) NOT NULL CHECK ( addressFrom <> '' ),
-    addressTo   VARCHAR(250) NOT NULL CHECK ( addressTo <> '' ),
-    subject     VARCHAR(250) NOT NULL CHECK ( subject <> '' ),
-    message     TEXT         NOT NULL CHECK ( message <> '' ),
-    created_at  TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE addresses
-(
-    address_id UUID PRIMARY KEY         DEFAULT uuid_generate_v4(),
-    email      VARCHAR(250) UNIQUE NOT NULL CHECK ( email <> '' ),
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    email_id     UUID PRIMARY KEY         DEFAULT uuid_generate_v4(),
+    address_from VARCHAR(250) NOT NULL CHECK ( address_from <> '' ),
+    address_to   VARCHAR(250) NOT NULL CHECK ( address_to <> '' ),
+    subject      VARCHAR(250) NOT NULL CHECK ( subject <> '' ),
+    message      TEXT         NOT NULL CHECK ( message <> '' ),
+    created_at   TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
 
 ALTER TABLE emails
     ADD COLUMN document_with_idx tsvector;
 UPDATE emails
-set document_with_idx = to_tsvector(emails.addressto || ' ' || emails.subject || ' ' || emails.message);
+set document_with_idx = to_tsvector(emails.address_to || ' ' || emails.subject || ' ' || emails.message);
 CREATE INDEX document_idx ON emails USING gin (document_with_idx);
 
 CREATE FUNCTION emails_tsvector_trigger() RETURNS trigger AS
 $$
 begin
-    new.document_with_idx := to_tsvector(new.addressto || ' ' || new.subject || ' ' || new.message);
+    new.document_with_idx := to_tsvector(new.address_to || ' ' || new.subject || ' ' || new.message);
     return new;
 end
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER tsvectorupdate BEFORE INSERT OR UPDATE
-    ON emails FOR EACH ROW EXECUTE PROCEDURE emails_tsvector_trigger();
+CREATE TRIGGER tsvectorupdate
+    BEFORE INSERT OR UPDATE
+    ON emails
+    FOR EACH ROW
+EXECUTE PROCEDURE emails_tsvector_trigger();
