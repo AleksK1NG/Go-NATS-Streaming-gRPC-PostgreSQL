10f3b98a1187dd8489bc416363c9419cc90dc7b5
Alexander Bryksin <alexander.bryksin@yandex.ru>
2021-03-11 16:43:47 +0300

    feat: add emails full text search

--- internal/email/delivery/http/v1/routes.go
+++ internal/email/delivery/http/v1/routes.go
@@ -3,6 +3,6 @@ package v1
 // MapRoutes products routes
 func (h *emailHandlers) MapRoutes() {
 	h.group.POST("", h.Create())
-	// h.group.GET("/:email_id", h.GetByID())
-	// h.group.POST("/search", h.Search())
+	h.group.GET("/:email_id", h.GetByID())
+	h.group.GET("/search", h.Search())
 }
