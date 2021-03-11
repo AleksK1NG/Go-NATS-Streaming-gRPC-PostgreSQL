5c77daec44e009548728138efdf2c2bade91f263
Alexander Bryksin <alexander.bryksin@yandex.ru>
2021-03-11 17:59:49 +0300

    feat: add migrations update

--- internal/email/repository/sql_queries.go
+++ internal/email/repository/sql_queries.go
@@ -1,17 +1,17 @@
 package repository
 
 const (
-	createEmailQuery = `INSERT INTO emails (addressfrom, addressto, subject, message) 
+	createEmailQuery = `INSERT INTO emails (address_from, address_to, subject, message) 
 	VALUES ($1, $2, $3, $4) 
-	RETURNING email_id, addressfrom, addressto, subject, message, created_at`
+	RETURNING email_id, address_from, address_to, subject, message, created_at`
 
-	getByIDQuery = `SELECT email_id, addressfrom, addressto, subject, message, created_at FROM emails WHERE email_id = $1`
+	getByIDQuery = `SELECT email_id, address_from, address_to, subject, message, created_at FROM emails WHERE email_id = $1`
 
 	searchTotalCountQuery = `SELECT count(email_id)
 	FROM emails
 	WHERE document_with_idx @@ to_tsquery($1)`
 
-	searchQuery = `SELECT email_id, addressto, addressfrom, subject, message, created_at
+	searchQuery = `SELECT email_id, address_to, address_from, subject, message, created_at
 	FROM emails
 	WHERE document_with_idx @@ to_tsquery($1) ORDER BY created_at OFFSET $2 LIMIT $3`
 )
